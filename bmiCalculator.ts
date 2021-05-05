938b6224f39e0282febe80c7bfe8103b6bf5bc28
Prabhakar Rai <student343.bhu@gmail.com>
2021-05-05 13:35:56 +0530

    9.5 bmi with express

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -3,7 +3,7 @@ interface PersonData {
   weight:number,
 }
 
-const calculateBmi = (data:PersonData):string => {
+export const calculateBmi = (data:PersonData):string => {
   const bmi:number = data.weight / ((data.height / 100) * (data.height / 100));
   if (bmi <= 18.5) {
     return 'Underweight';
@@ -24,6 +24,19 @@ const parseArgumentsBmi = (args:Array<string>):PersonData => {
     return {
       height: Number(args[2]),
       weight: Number(args[3]),
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+export const parseArgumentsBmiExpress = (args:Array<string>):PersonData => {
+  if (args.length != 2) throw new Error('Not enough arguments');
+
+  if (!isNaN(Number(args[0])) && !isNaN(Number(args[0]))) {
+    return {
+      height: Number(args[0]),
+      weight: Number(args[1]),
     }
   } else {
     throw new Error('Provided values were not numbers!');
--- index.ts
+++ index.ts
@@ -1,10 +1,36 @@
 import express from 'express';
+import { calculateBmi, parseArgumentsBmiExpress } from './bmiCalculator'
 const app = express();
 
 const PORT = 3001;
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  try {
+    if (!req.query['height'] || !req.query['weight']) {
+      throw new Error('height & weight query params are required!');
+    }
+    const height:string = String(req.query['height']);
+    const weight:string = String(req.query['weight']);
+    const params = parseArgumentsBmiExpress([height, weight]);
+    const bmiResult = calculateBmi(params);
+    res.json({
+      height: params.height,
+      weight: params.weight,
+      bmi: bmiResult,
+    });
+  } catch (e) {
+    res.status(400).json({
+      error: {
+        message: 'malformatted parameters',
+      },
+      usage: '/bmi?weight=70&height=170',
+    });
+    console.error(e.message, req.query);
+  }
 });
 
 app.listen(PORT, () => {
